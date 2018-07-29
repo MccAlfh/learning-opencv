c49a2c74480e68b60ad890ccc245d098cbe50636
Nazarii Bardiuk <nazarii.bardiuk@protonmail.com>
2018-07-29 12:41:13 +0100

    add help to executable

--- src/example_02_01.hs
+++ src/example_02_01.hs
@@ -6,7 +6,17 @@ import           System.Environment
 main :: IO ()
 main = do
     args <- getArgs
-    img <- CV.imdecode CV.ImreadUnchanged <$> B.readFile (head args)
-    CV.withWindow "Example 2-1" $ \window -> do
-        CV.imshow window img
-        void $ CV.waitKey 0
+    case args of
+      [] -> help
+      file : _ -> do
+        img <- CV.imdecode CV.ImreadUnchanged <$> B.readFile file
+        CV.withWindow "Example 2-1" $ \window -> do
+          CV.imshow window img
+          void $ CV.waitKey 0
+    where
+      help = do
+        name <- getProgName
+        putStrLn "A simple OpenCV program that loads and displays an image from disk"
+        putStrLn $ name ++ " <path/filename>"
+        putStrLn "For example:"
+        putStrLn $ name ++ " ./resources/fruits.jpg"
